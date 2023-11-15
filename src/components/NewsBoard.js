f8372fdb54c3d917c4b49c0bb4bf26d6d30964fd
storm <stormarasa2@gmail.com>
2023-11-15 20:36:18 +0300

    experimented with different API's then edited my newsboard and newsitem component

--- src/components/NewsBoard.js
+++ src/components/NewsBoard.js
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from 'react'
+import NewsItem from './NewsItem';
 
 function NewsBoard() {
     const [articles, setArticles] = useState([]);
 
     useEffect(() => {
         const apiKey = `pub_32928ca1fdc2fdf4407a760abf3535c057454`
-      let url = ` https://newsdata.io/api/1/news?apikey= ${apiKey} &country=ke&language=en&category=business,health,politics,top,world `
+      let url = ` https://newsdata.io/api/1/news?country=ke&category=top&apikey=${apiKey}`
       fetch(url).then(res=> res.json())
-      .then((data) => setArticles(data.articles))
+      .then((data) => setArticles(data.results))
+      .catch((error) => console.error('Error fetching articles:', error));
     }, [])
     
   return (
     <div>
-        <h2 className='text-center'> Latest 
-        <span className='badge bg-danger'>News</span>  {/* added color to news */}
+        <h2 className='text-center'>Latest <span className='badge bg-danger'>News</span>  {/* added color to news */}
         </h2>
+        {articles.map((news, index) => (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description} 
+            src={news.image_url} 
+            url={news.link}
+          />
+         ))}
+        
+        
     </div>
   )
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function NewsItem() {
+export default function NewsItem({title, description, src, url}) {
   return (
-    <div>
-        
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 px-2" style={{maxWidth:"345px"}}>
+    <img src={src} style = {{height:"200px", width:"360px"}}className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{description.slice(0, 50)}</p>
+      <a href= {url} className="btn btn-primary">Read More...</a>
     </div>
+  </div>
   )
 }
